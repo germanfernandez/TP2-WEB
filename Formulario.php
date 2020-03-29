b863f268285cdbeb4982006a35a3bad6b946f62e
germanfernandez <germanfernandez@users.noreply.github.com>
2020-03-29 02:29:13 -0300

    Ejercicio 1-2

---
+++ Formulario.php
@@ -0,0 +1,24 @@
+<?php
+    $nombre = $_POST[nombre];
+    $email = $_POST[email];
+    $tel = $_POST[tel];
+    $edad = $_POST[edad];
+    $talla = $_POST[talla];
+    $altura = $_POST[altura];
+    $nacimiento = $_POST[nacimiento];
+    $cpelo = $_POST[cpelo];
+    $diaturno = $_POST[diaturno];
+    $horaturno = $_POST[horaturno];
+
+    echo "<h2>Informacion recibida sobre el turno solicitado</h2>";
+    echo "Nombre: " . $nombre . "<br/>";
+    echo "Email: " . $email . "<br/>";
+    echo "Telefono: " . $tel . "<br/>";
+    echo "Edad: " . $edad . "<br/>";
+    echo "Talla de Pie: " . $talla . "<br/>";
+    echo "Altura: " . $altura . "<br/>";
+    echo "Nacimiento: " . $nacimiento . "<br/>";
+    echo "Color de Pelo: " . $cpelo . "<br/>";
+    echo "Dia del Turno: " . $diaturno . "<br/>";
+    echo "Horario del Turno: " . $horaturno . "<br/>";
+?>
