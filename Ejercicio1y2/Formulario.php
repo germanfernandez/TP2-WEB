512de3b22054cef0af36f2e24fb35a741545453a
dbm349 <delmore97@hotmail.com>
2020-04-07 16:09:18 -0300

    uso del framework

--- Ejercicio1y2/Formulario.php
+++ Ejercicio1y2/Formulario.php
@@ -22,36 +22,4 @@
         echo "Dia del Turno: " . $fechaturno . "<br/>";
         echo "Horario del Turno: " . $horaturno . "<br/>";
 
-    if(isset($_POST['submit'])){
-        if(empty($nombre)){
-            echo "<p class='error'> * Campo nombre incompleto </p>";
-        }
-
-        if(empty($email)){
-            echo "<p class='error'> * Campo email incompleto </p>";
-        }else{
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-            echo "<p class='error'> * Email incorrecto </p>";
-        }
-
-        if(empty($tel)){
-            echo "<p class='error'> * Campo teléfono incompleto </p>";
-        }else{
-            if(!is_numeric($tel)){
-                echo "<p class='error'> * Solo se permiten números </p>";
-            }
-        }
-
-        if(empty($nacimiento)){
-            echo "<p class='error'> * Campo fecha de nacimiento incompleto </p>";
-        }
-
-        if(empty($fechaturno)){
-            echo "<p class='error'> * Campo fecha de turno incompleto </p>";
-        }
-
-        if(($talla<20) || ($talla>45)){
-            echo "<p class='error'> * La talla debe ser mayor a 20 y menor a 45 </p>";
-        }
-    }
 ?>
