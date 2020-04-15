a9f54197b4dd7b6e249cf0ef43263e4762119392
dbm349 <delmore97@hotmail.com>
2020-04-15 15:12:50 -0300

    nuevos cambios

--- Ejercicio4/Formulario.php
+++ Ejercicio4/Formulario.php
@@ -13,6 +13,7 @@
         $horaturno = $_POST['horaturno'];
     
         $fturno=strtotime($fechaturno);
+        $fnacimiento=strtotime($nacimiento);
         $fechaactual= strtotime(date("d-m-Y H:i:00",time()));
 
 
@@ -49,6 +50,7 @@
                     echo "<p class='error'> * Email incorrecto </p>";
                 }
             
+
                 if(empty($tel)){
                     echo "<p class='error'> * Campo teléfono incompleto </p>";
                 }else{
@@ -57,30 +59,66 @@
                     }
                 }
             
-                if(empty($horaturno)){
-                    echo "<p class='error'> * Campo horario de Turno incompleto </p>";
-                }elseif((!preg_match("/^(0[8-9]|1[0-6]):(00|15|30|45)$/",$horaturno)) &&
+                if((!is_numeric($edad)){
+                    echo "<p> *La edad debe ser numérica </p>"
+                }else{
+                    if(($edad<0) || ($edad>105)){
+                        echo "<p class='error'> * La edad debe ser mayor a 0 y menor a 105 </p>";
+                    }
+                }
+
+
+                if((!preg_match("/^(0[8-9]|1[0-6]):(00|15|30|45)$/",$horaturno)) &&
                          (!preg_match("/^(17):(00)$/",$horaturno))){
                     echo "<p class='error'> * Turno no válido </p>";
                 }
                 
                 
-
                 if(empty($nacimiento)){
                     echo "<p class='error'> * Campo fecha de nacimiento incompleto </p>";
+                }else{
+                    if(!preg_match("/^([0-9][0-9][0-9][0-9])-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$/",$nacimiento)){
+                        echo "<p class='error'> * Formato de fecha inválida </p>";
+                    }
+                }else{
+                    if($fnacimiento<$fechaactual){
+                        echo "<p class='error'> * ERROR! Fecha de nacimiento incorrecta</p>";
+                    }
                 }
             
+
+
+
                 if(empty($fechaturno)){
                     echo "<p class='error'> * Campo fecha de turno incompleto </p>";
+                }else{
+                    if(!preg_match("/^([0-9][0-9][0-9][0-9])-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$/",$nacimiento)){
+                        echo "<p class='error'> * Formato de fecha inválida </p>";
+                    }
                 }else{
                     if($fturno<$fechaactual){
                         echo "<p class='error'> * La fecha del turno debe ser mayor o igual a la actual</p>";
                     }
                 }
             
-                if(($talla<20) || ($talla>45)){
-                    echo "<p class='error'> * La talla debe ser mayor a 20 y menor a 45 </p>";
-                }
+
+                if((!is_numeric($altura)){
+                    echo "<p> *La altura debe ser numérica </p>"
+                }else{
+                    if(($altura<0.5) || ($altura>2.5)){
+                        echo "<p class='error'> * La altura debe ser mayor a 0.5 y menor a 2.5 </p>";
+                    }
+                }    
+                    
+                
+                if((!is_numeric($talla)){
+                    echo "<p> *La altura debe ser numérica </p>"
+                }else{
+
+                    if(($talla<20) || ($talla>45)){
+                        echo "<p class='error'> * La talla debe ser mayor a 20 y menor a 45 </p>";
+                    }
+                } 
 
 
                 if(($cpelo !="Castaño") &&
